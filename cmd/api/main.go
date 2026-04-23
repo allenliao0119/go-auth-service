ea2aa77682a31772d7985f32e500a90917ec2704
Allen Liao <allen10153@gmail.com>
2026-04-23 14:32:53 +0800

    feat(cmd/api): start HTTP server with OS signal graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/allenliao0119/go-auth-service/internal/application"
 	"github.com/allenliao0119/go-auth-service/internal/config"
+	"github.com/allenliao0119/go-auth-service/internal/http"
 )
 
 func main() {
@@ -18,5 +23,17 @@ func main() {
 		log.Fatalf("failed to create application: %v", err)
 	}
 
-	log.Println(app)
+	server, err := http.NewServer(app)
+	if err != nil {
+		app.Logger.Error(context.Background(), "failed to create server", "error", err)
+		os.Exit(1)
+	}
+
+	server.Start()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	server.Shutdown()
 }
